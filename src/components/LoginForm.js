a2c1ac367c0878ffea14d8fbd2c24bfc9c9cec04
ruthmesfun <ruth.mesfun@gmail.com>
2019-01-09 09:00:09 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -19,7 +19,7 @@ class LoginForm extends React.Component {
   handleSubmit = (event) => {
     event.preventDefault()
     if (this.state.username && this.state.password){
-      return this.state
+      this.props.onSubmit(this.state)
     }
   }
 
