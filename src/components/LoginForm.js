1f22c9666959f03426119880862a5b94b8d879d1
GermainDuran <arturodurantorres@gmail.com>
2019-01-10 20:43:22 -0500

    Done.

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,22 +4,52 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username: "",
+      password: ""
+    };
   }
+
+HandleSubmit = (e) => {
+  e.preventDefault()
+  // ✓ should call the prevent the default action when the form is being submitted (47ms)
+  //     ✓ should call the `onSubmit` callback prop when the form is being submitted
+   if (this.state.username !== '' && this.state.password !== '') {
+     this.props.onSubmit(this.state)
+   }
+
+}
+
+handleChange = (e) => {
+  this.setState({
+    username: e.target.value
+    // estp si quisiera unir los chage handle en uno [e.target.name]: event.target.value
+   })
+}
+
+handleChange1 = (e) => {
+  this.setState({
+    password: e.target.value
+  })
+}
+
+
+
+
 
   render() {
     return (
-      <form>
+      <form onSubmit={this.HandleSubmit}>
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input onChange={this.handleChange} id="username" name="username" type="text" />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input onChange={this.handleChange1} id="password" name="password" type="password"   />
           </label>
         </div>
         <div>
@@ -29,5 +59,6 @@ class LoginForm extends React.Component {
     );
   }
 }
+
 
 export default LoginForm;
--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -4,14 +4,24 @@ class TwitterMessage extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      valorDelmensaje: ""
+    };
   }
 
+  handleEvent = (event) => {
+    this.setState({
+      valorDelmensaje: event.target.value
+    })
+  }
+
+//aqui utilizo .props.maxChars porq es una propiedad que viene del padre
   render() {
     return (
       <div>
-        <strong>Your message:</strong>
-        <input type="text" />
+        <strong>Your valorDelmensaje:</strong>
+        <input onChange={this.handleEvent} value={this.state.valorDelmensaje} type="text" />
+         <p>Caracteres: {this.props.maxChars - this.state.valorDelmensaje.length}</p>
       </div>
     );
   }
